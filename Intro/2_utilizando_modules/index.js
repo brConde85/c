d7cb7669fe134beb0ea376d2f3999dedb6866ac4
Bruno <bruno.panho@hop.digital>
2021-12-06 11:10:48 -0300

    Adiçao de repositorio referente ao curso de Nodejs

---
+++ Intro/2_utilizando_modules/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs'); // file system
+
+fs.readFile('arquivo.txt', 'utf-8', (error, data) => {
+  if (error) {    
+    return console.log(error);
+  }
+  
+  console.log(data);
+});
